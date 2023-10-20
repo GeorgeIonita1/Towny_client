52687047460ec5071d2aae270678d46c30814b9b
George <g3orrg301@gmail.com>
2023-10-20 21:52:11 +0300

    Create aside menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { Container  } from '@mui/material';
+import { Container } from '@mui/material';
+
 import { SectionElement } from '../mui/CustomComponents';
 import MainMenu from '../components/main-menu/MainMenu';
 
